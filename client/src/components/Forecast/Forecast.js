50b2a68c0dd712f71cd2e7043283ed4759f0f991
James McCarron <73326512+jimmikmak@users.noreply.github.com>
2021-04-16 11:47:33 +1000

    Added fetch for Forecast

---
+++ client/src/components/Forecast/Forecast.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+
+const Forecast = () => {
+  const [responseObj, setResponseObj] = useState({});
+  function getForecast() {
+    fetch(
+      "https://community-open-weather-map.p.rapidapi.com/weather?q=Sydney",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key":
+            "18b0b2afc4mshc736424a2c7d6c3p16c6d8jsnd3bf40f8cfd9",
+          "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+        },
+      }
+    )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  return (
+    <div>
+      <h2>Find Current Weather Conditions</h2>
+      <div>{JSON.stringify(responseObj)}</div>
+      <Button variant="contained" color="primary" onClick={getForecast}>
+        Get Forecast
+      </Button>
+    </div>
+  );
+};
+export default Forecast;
