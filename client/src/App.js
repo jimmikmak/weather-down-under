1b954ebd20fbe6be664e34dedd00a97b0a9a5157
James McCarron <73326512+jimmikmak@users.noreply.github.com>
2021-04-18 15:49:04 +1000

    response codes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
+import React from "react";
 import "./App.css";
+import Forecast from "./components/Forecast/Forecast";
 
 function App() {
   return (
@@ -6,7 +8,9 @@ function App() {
       <header className="App-header">
         <h1>Weather Down Under</h1>
       </header>
-      <main></main>
+      <main>
+        <Forecast />
+      </main>
       <footer>Page created by James McCarron</footer>
     </div>
   );
