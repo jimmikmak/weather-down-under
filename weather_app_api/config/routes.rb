5a3eea7d7bf15e4fe47c791e69ba2f465ce69d94
James McCarron <73326512+jimmikmak@users.noreply.github.com>
2021-04-25 13:27:24 +1000

    api route tested successfully

--- weather_app_api/config/routes.rb
+++ weather_app_api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/welcome', to: 'welcome#index'
   namespace :api do
-  resources :bookmarks
+    resources :bookmarks
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
