c262f9faee9ac5e35c07949ab487b1111aee1a35
James McCarron <73326512+jimmikmak@users.noreply.github.com>
2021-04-25 14:01:53 +1000

    added user route

--- weather_app_api/config/routes.rb
+++ weather_app_api/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/welcome', to: 'welcome#index'
   namespace :api do
     resources :bookmarks
+    resources :users
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
