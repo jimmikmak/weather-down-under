57fd190f072f1468e3b08a78ac3495d9ac37b99c
James McCarron <73326512+jimmikmak@users.noreply.github.com>
2021-04-26 11:16:05 +1000

    auth login route

--- weather_app_api/config/routes.rb
+++ weather_app_api/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/welcome', to: 'welcome#index'
   namespace :api do
+    post 'auth/login', to: 'auth#login'
     resources :bookmarks
     resources :users
   end
