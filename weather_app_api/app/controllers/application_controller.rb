88184a60249ff6826f5b1e7187ac019e30f8445a
James McCarron <73326512+jimmikmak@users.noreply.github.com>
2021-04-26 13:19:55 +1000

    successful login

--- weather_app_api/app/controllers/application_controller.rb
+++ weather_app_api/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+    def encode_token user_id
+        JWT.encode(user_id, 'top-secret-password-!@#$%')
+    end
+    
+    def decode_token
+    end
+
 end
