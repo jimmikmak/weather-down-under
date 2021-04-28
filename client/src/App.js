7aec8a8efc343ef5536bd936c5b7f2a0679f36b1
James McCarron <73326512+jimmikmak@users.noreply.github.com>
2021-04-28 11:28:53 +1000

    npm audit fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "./App.css";
 import Forecast from "./components/Forecast/Forecast";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Weather Down Under</h1>
-      </header>
-      <main>
-        <Forecast />
-      </main>
-      <footer>Page created by James McCarron</footer>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <header className="App-header">
+            <h1>Weather Down Under</h1>
+          </header>
+          <main>
+            <Forecast />
+          </main>
+          <footer>Page created by James McCarron</footer>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
