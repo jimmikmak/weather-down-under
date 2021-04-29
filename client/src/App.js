1be4bb700931ae3c98e05977de17488b5199e118
James McCarron <73326512+jimmikmak@users.noreply.github.com>
2021-04-30 09:40:47 +1000

    setLoggedIn

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,33 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
 import { Forecast } from "./components/Forecast/Forecast";
 import { UserForm } from "./components/User/UserForm";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <BrowserRouter>
       <div className="App">
+        {/* find the syntax for Link and add a link for a user to click login */}
+        <Link to="/login" />
         <Switch>
           <Route exact path="/login">
-            <UserForm />
+            <UserForm setLoggedIn={setLoggedIn} />
+          </Route>
+          <Route exact path="/forecast">
+            <Forecast loggedIn={loggedIn} />
           </Route>
         </Switch>
-
         <header className="App-header">
           <h1>Weather Down Under</h1>
         </header>
-
         <main>
           <Forecast />
         </main>
-
         <footer>Page created by James McCarron</footer>
       </div>
+      )
     </BrowserRouter>
   );
 }
