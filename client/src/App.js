abc6646359f45e7e990e7cf91a2af58c82d11a85
James McCarron <73326512+jimmikmak@users.noreply.github.com>
2021-04-30 15:14:27 +1000

    unhandled rejection error

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 import "./App.css";
 import { Forecast } from "./components/Forecast/Forecast";
@@ -6,11 +7,17 @@ import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+
+  // const logInForm = React.forwardRef((props, ref) => (
+  //   <a ref={ref} {...props}>
+  //     {props.children}
+  //   </a>
+  // ));
   return (
     <BrowserRouter>
       <div className="App">
         {/* find the syntax for Link and add a link for a user to click login */}
-        <Link to="/login" />
+        <Link to="/login" exact component={UserForm}></Link>
         <Switch>
           <Route exact path="/login">
             <UserForm setLoggedIn={setLoggedIn} />
