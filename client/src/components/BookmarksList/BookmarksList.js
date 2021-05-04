0757f1546c6f0c1cc0a9af119536bbde1fef9521
James McCarron <mccarron.j@gmail.com>
2021-05-04 20:54:07 +1000

    bookmark list rendered

--- client/src/components/BookmarksList/BookmarksList.js
+++ client/src/components/BookmarksList/BookmarksList.js
@@ -1,47 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 
 export const BookmarksList = () => {
-  const [bookMarksList, setBookMarksList] = useState([]);
-  let [city, setCity] = useState("");
-  let [unit, setUnit] = useState("imperial");
-  let [responseObj, setResponseObj] = useState({});
-  let [error, setError] = useState(false);
-  let [loading, setLoading] = useState(false);
-
-  function getForecast(city) {
-    const uriEncodedCity = encodeURIComponent(city);
-
-    fetch(
-      `https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`,
-      {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-          "x-rapidapi-key": process.env.REACT_APP_API_KEY,
-        },
-      }
-    )
-      // after the data is received from the API (*asynchronous)
-      .then((response) => response.json())
-      .then((response) => {
-        if (response.cod !== 200) {
-          throw new Error();
-        }
-        setResponseObj(response);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(true);
-        setLoading(false);
-        console.log(err.message);
-      });
-  }
+  const [bookMarks, setBookMarks] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:3000/api/bookmarks`)
+    fetch("http://localhost:3000/api/bookmarks", {
+      headers: {
+        "Content-Type": "application/json",
+        token: window.localStorage.getItem("token"),
+      },
+    })
       .then((response) => response.json())
-      .then((data) => console.log("todos:", data));
+      .then((data) => setBookMarks(data));
   }, []);
 
   return (
@@ -49,13 +19,15 @@ export const BookmarksList = () => {
       <header className="App-header">
         <h1>Bookmarks</h1>
       </header>
-      {bookMarksList.map((bookmark) => (
-        <div>
-          <div>{bookmark.city}</div>
-          <div>{getForecast(bookmark.city)}</div>
-        </div>
-      ))}
-      <footer>Page created by James McCarron</footer>
+      <ul>
+        {bookMarks.map((el) => (
+          <li key={el.id}>{el.city}</li>
+        ))}
+      </ul>
+      <footer className="App-footer">
+        <p>Page created by James McCarron</p>
+        <p>Copyright &copy; 2021 by Weather Down Under. All rights reserved.</p>
+      </footer>
     </div>
   );
 };
