2934226f595e5c0bb82fcd1b3b0e4649649c818c
James McCarron <mccarron.j@gmail.com>
2021-05-04 21:22:34 +1000

    bookmark icons

--- client/src/components/BookmarksList/BookmarksList.js
+++ client/src/components/BookmarksList/BookmarksList.js
@@ -1,4 +1,15 @@
 import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Grid from "@material-ui/core/Grid";
+import FolderIcon from "@material-ui/icons/Folder";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 export const BookmarksList = () => {
   const [bookMarks, setBookMarks] = useState([]);
@@ -14,20 +25,70 @@ export const BookmarksList = () => {
       .then((data) => setBookMarks(data));
   }, []);
 
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      maxWidth: 752,
+    },
+    demo: {
+      backgroundColor: theme.palette.background.paper,
+    },
+    title: {
+      margin: theme.spacing(4, 0, 2),
+    },
+  }));
+
+  function generate(element) {
+    return [0, 1, 2].map((value) =>
+      React.cloneElement(element, {
+        key: value,
+      })
+    );
+  }
+
+  const classes = useStyles();
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+
   return (
-    <div>
-      <header className="App-header">
-        <h1>Bookmarks</h1>
-      </header>
-      <ul>
-        {bookMarks.map((el) => (
-          <li key={el.id}>{el.city}</li>
-        ))}
-      </ul>
+    <div className={classes.root}>
+      <Grid item xs={12} md={6}>
+        <header className="App-header">
+          <h1>Bookmarks</h1>
+        </header>
+        <div className={classes.demo}>
+          <List dense={dense}>
+            {generate(
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <FolderIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Single-line item"
+                  secondary={secondary ? "Secondary text" : null}
+                />
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            )}
+          </List>
+        </div>
+      </Grid>
       <footer className="App-footer">
         <p>Page created by James McCarron</p>
         <p>Copyright &copy; 2021 by Weather Down Under. All rights reserved.</p>
       </footer>
     </div>
+
+    // <ul>
+    //   {bookMarks.map((el) => (
+    //     <li key={el.id}>{el.city}</li>
+    //   ))}
+    // </ul>
   );
 };
